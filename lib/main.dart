4ad47b73813c48e8e57284454979977f582a47c6
Anishabibah1 <anishabibah491@gmail.com>
2026-06-03 19:18:01 +0800

    struktur folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,9 @@
 import 'package:flutter/material.dart';
-import 'theme.dart';
-import 'splash_screen.dart';
 
 void main() {
-  runApp(const FoodieGoApp());
-}
-
-class FoodieGoApp extends StatelessWidget {
-  const FoodieGoApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FoodieGo',
-      debugShowCheckedModeBanner: false,
-      theme: foodieTheme(),
-      home: const SplashScreen(),
-    );
-  }
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Center(child: Text('FoodieGoo siap!')),
+    ),
+  ));
 }
